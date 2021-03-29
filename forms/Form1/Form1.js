12403edbbbff66a446a18e0e0ba29c52fd3a421d
Makala Smith <mms96354@creighton.edu>
2021-03-29 10:37:17 -0500

    changes

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -7,21 +7,20 @@ let netID = "mms96354"
 
 
 btnCustomers.onclick=function(){
-  query = "SELECT * FROM customer
+  query = "SELECT * FROM group-A1.customer"
     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=mms96354&query=" + query)
 
     if (req.status == 200) { //transit trip worked. 
         results = JSON.parse(req.responseText)
         if (results.length == 0)    
-           lblMessage2.textContent = "There are no customers in the database."
+           lblMessage.value = "There are no customers in the database."
         else {        
            let message = ""
            for (i = 0; i < results.length; i++)
                message = message + results[i][1] + "\n"
-           txtTypeNames.value = message
+           txtaUsers.value = message
         } // end else
 
     } else   // the transit didn't work - bad wifi? server turned off?
-        lblMessage2.textContent = "Error code: " + req.status
-}
+        lblMessage.value = "Error code: " + req.status
 }
